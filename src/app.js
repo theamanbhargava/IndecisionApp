08fb33842df0ebbc4c6d8fcc778856956c46bc4c
theamanbhargava <aman56493@gmail.com>
2017-10-11 17:31:33 +0530

    Migrated to const entirely for indecision app and add playground folder

--- src/app.js
+++ src/app.js
@@ -70,28 +70,24 @@ const Action = (props) => {
     );
 };
 
-class Options extends React.Component {
-    render() {
-        return (
-            <div>
-                <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-                {
-                    this.props.options.map((option) => <Option key={option} optionText={option} />)
-                }
-            </div>
-        );
-    }
-}
+const Options = (props) => {
+    return (
+        <div>
+            <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {
+                props.options.map((option) => <Option key={option} optionText={option} />)
+            }
+        </div>
+    );
+};
 
-class Option extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.optionText}
-            </div>
-        );
-    }
-}
+const Option = (props) => {
+    return (
+        <div>
+            {props.optionText}
+        </div>
+    );
+};
 
 class AddOption extends React.Component {
     constructor(props){
